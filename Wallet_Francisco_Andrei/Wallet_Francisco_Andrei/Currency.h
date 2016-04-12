a0e5835a73fdfb109fc95d1088b9db28dafd8d90
franalvacad <franalvacad@gmail.com>
2016-04-12 16:00:03 -0700

    made base class

--- Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Currency.h
+++ Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Currency.h
@@ -1,9 +1,22 @@
 #pragma once
+#include <string>
 class Currency
 {
 private:
-
+	int whole, frac;
+	std::string name, fracName;
 public:
+	//sets
+	void setWhole();
+	void setFrac();
+	void setName();
+	void setFracName();
+	//gets
+	int getWhole();
+	int getFrac();
+	std::string getName;
+	std::string getFracName();
+
 	//virtual something, add later
 };
 
