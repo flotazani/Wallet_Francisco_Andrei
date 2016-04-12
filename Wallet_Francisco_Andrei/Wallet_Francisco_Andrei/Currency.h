83cc32d0e3acd02e6c34d426fa414b0e3e1d6819
franalvacad <franalvacad@gmail.com>
2016-04-12 15:33:04 -0700

    created classes

--- Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Currency.h
+++ Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Currency.h
@@ -1,26 +1,10 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-class Currency{
+#pragma once
+class Currency
+{
 private:
-	struct Pounds{
-		int Pounds;
-		int pence;
-	};
-	struct Rub{
-		int Rub;
-		int kopeika;
-	};
-	struct Euro{
-		int Euro;
-		int cent;
-	};
-	struct Dollar{
-		int Dollar;
-		int centD;
-	};
-public:
-	friend Pounds operator+(const)
 
+public:
+	Currency();
+	~Currency();
 };
+
