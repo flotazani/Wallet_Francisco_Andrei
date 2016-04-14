da393364e57daade1da8a691d02e641755ac321a
flotazani <flotzani@mail.ru>
2016-04-14 00:45:17 -0700

    constructors

--- Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Wallet.h
+++ Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Wallet.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "Currency.h"
+#include "Dollar.h"
+#include "Euro.h"
+#include "Franc.h"
+#include "Hryvnia.h"
+#include "Ruble.h"
+
 using namespace std;
 class Wallet
-{
+{ // you have to put enums somewhere so we will be able to call elements ptr[Dollar] instead of ptr[2]
 private:
 	Currency *ptr[5]; 
 public:
 	Wallet();
+	Wallet(int, double);
+	Wallet(int, int, int);
 	~Wallet();
 };
 
