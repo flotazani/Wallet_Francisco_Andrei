7bf1853a571ec7a7c9c4568f2bb191a5882ecac9
franalvacad <franalvacad@gmail.com>
2016-04-14 00:45:33 -0700

    menu

--- Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Main.cpp
+++ Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Main.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
+#include <string.h>
 #include <iomanip>
-#include "Franc.h";
+#include "Wallet.h"
 using namespace std;
 
 int main(){
-	Franc jakoor(4.13);
+	char selec;
+	bool mainLoop = 0;
 
-	cout << jakoor.getWhole() << " " << jakoor.getFrac();
+	while (!mainLoop)
+	{
+		cout <<
+			"Welcome to your Wallet" << endl <<
+			"1: Add funds" << endl <<
+			"2: Check Funds" << endl <<
+			"3: Exit" << endl << endl <<
+			"Your choice: ";
 
+		cin >> selec;
+
+		switch (selec)
+		{
+		default:
+			cout << "Please enter a valid selection!";
+			break;
+		case '1':
+			break;
+		case '2':
+			break;
+		case '3':
+			cout << "Goodbye!";
+			mainLoop = true;
+			break;
+		}
+	}
 	system("pause>nul");
 	return 0;
+}
+
+void addFunds()
+{
+
+}
+
+void checkFunds()
+{
+
 }
--- Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Wallet.h
+++ Wallet_Francisco_Andrei/Wallet_Francisco_Andrei/Wallet.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <stdlib.h>
+#include <time.h>
+#include "Dollar.h"
+#include "Euro.h"
+#include "Franc.h"
+#include "Hryvnia.h"
+#include "Ruble.h"
+
+enum money
+{
+	Dollar, Euro, Franc, Hryvnia, Ruble
+};
 class Wallet
 {
+private:
+	bool isEmpty, theresDollar, theresEuro, theresFranc, theresHryvnia, theresRuble;
+	int randNum;
+
 public:
-	Wallet();
+	Wallet()
+	{
+		srand(time(NULL));
+		
+	}
 	~Wallet();
 };
-
